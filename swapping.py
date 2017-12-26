c0baedefd5dcaf89e2ee29a63b324eea2f0fe33a
kalpanaDevOps <kalpana@localhost.localdomain>
2017-12-26 04:38:13 -0800

    Adding python files into directory

---
+++ swapping.py
@@ -0,0 +1,18 @@
+#!usr/bin/python
+
+# Python program to swap two variables
+
+
+# x = input('Enter value of x: ')
+# y = input('Enter value of y: ')
+
+x = 5
+y = 10
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
